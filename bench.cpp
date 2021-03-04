d8c4c3492f6b409ea64aedcea98ae2116204020b
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-03-04 22:41:52 +0000

    switch benchmarks to google benchmark

--- bench.cpp
+++ bench.cpp
@@ -1,137 +1,98 @@
-#include "criterion/criterion.hpp"
+// #include "criterion/criterion.hpp"
 #include "thh_handles/thh_handles.hpp"
 
-BENCHMARK(AddElement)
-{
-  SETUP_BENCHMARK(
-    thh::container_t<int> container;
-  )
- 
-  // benchmarking
-  const thh::handle_t handle = container.add();
-  
-  TEARDOWN_BENCHMARK(
-    container.remove(handle);
-  )
-}
+#include <benchmark/benchmark.h>
 
-BENCHMARK(AddElementWithReserve)
-{
-  SETUP_BENCHMARK(
-    thh::container_t<int> container;
-    container.reserve(8);
-  )
- 
-  // benchmarking
-  const thh::handle_t handle = container.add();
-  
-  TEARDOWN_BENCHMARK(
-    container.remove(handle);
-  )
-}
-
-BENCHMARK(RemoveElement)
-{
-  SETUP_BENCHMARK(
-    thh::container_t<int> container;
+static void AddElement(benchmark::State& state) {
+  thh::container_t<int> container;
+  for (auto _ : state) {
     const thh::handle_t handle = container.add();
-  )
- 
-  // benchmarking
-  container.remove(handle);
-  
-  TEARDOWN_BENCHMARK(
-    // nothing
-  )
-}
-
-BENCHMARK(HasElementPresent)
-{
-  SETUP_BENCHMARK(
-    thh::container_t<int> container;
-    thh::handle_t handle = container.add();
-  )
- 
-  // benchmarking
-  container.has(handle);
-  
-  TEARDOWN_BENCHMARK(
-    container.remove(handle);
-  )
-}
-
-BENCHMARK(HasElementNotPresent)
-{
-  SETUP_BENCHMARK(
-    thh::container_t<int> container;
-    thh::handle_t handle = container.add();
-    container.remove(handle);
-  )
- 
-  // benchmarking
-  container.has(handle);
-  
-  TEARDOWN_BENCHMARK(
-    // nothing
-  )
-}
-
-BENCHMARK(Resolve)
-{
-  SETUP_BENCHMARK(
-    thh::container_t<int> container;
-    thh::handle_t handle = container.add();
-  )
- 
-  // benchmarking
-  int* element = container.resolve(handle);
-  
-  TEARDOWN_BENCHMARK(
-    container.remove(handle);
-  )
-}
-
-BENCHMARK(EnumerateCallback)
-{
-  SETUP_BENCHMARK(
-    thh::container_t<int> container;
-    std::vector<thh::handle_t> handles;
-    for (int i = 0; i < 10; ++i) {
-      handles.push_back(container.add());
-    }
-  )
-
-  container.enumerate([i = 0](auto& element) mutable {
-    element = i++;
-  });
-
-  TEARDOWN_BENCHMARK(
-    for (int i = 0; i < container.size(); ++i) {
-      container.remove(handles[i]);
-    }
-  )
-}
-
-BENCHMARK(EnumerateResolve)
-{
-  SETUP_BENCHMARK(
-    thh::container_t<int> container;
-    std::vector<thh::handle_t> handles;
-    for (int i = 0; i < 10; ++i) {
-      handles.push_back(container.add());
-    }
-  )
-
-  for (int i = 0; i < container.size(); ++i) {
-    int* element = container.resolve(handles[i]);
-    *element = i;
+    benchmark::DoNotOptimize(handle);
   }
-
-  TEARDOWN_BENCHMARK(
-    for (int i = 0; i < container.size(); ++i) {
-      container.remove(handles[i]);
-    }
-  )
 }
 
-CRITERION_BENCHMARK_MAIN()
+BENCHMARK(AddElement);
+
+static void AddElementWithReserve(benchmark::State& state) {
+  thh::container_t<int> container;
+  container.reserve(8);
+  for (auto _ : state) {
+    const thh::handle_t handle = container.add();
+    benchmark::DoNotOptimize(handle);
+  }
+}
+
+BENCHMARK(AddElementWithReserve);
+
+static void RemoveElement(benchmark::State& state) {
+  thh::container_t<int> container;
+  const thh::handle_t handle = container.add();
+  for (auto _ : state) {
+    container.remove(handle);
+  }
+}
+
+BENCHMARK(RemoveElement);
+
+static void HasElementPresent(benchmark::State& state) {
+  thh::container_t<int> container;
+  thh::handle_t handle = container.add();
+  for (auto _ : state) {
+    container.has(handle);
+  }
+}
+
+BENCHMARK(HasElementPresent);
+
+static void HasElementNotPresent(benchmark::State& state) {
+  thh::container_t<int> container;
+  thh::handle_t handle = container.add();
+  container.remove(handle);
+  for (auto _ : state) {
+    container.has(handle);
+  }
+}
+
+BENCHMARK(HasElementNotPresent);
+
+static void Resolve(benchmark::State& state) {
+  thh::container_t<int> container;
+  thh::handle_t handle = container.add();
+  for (auto _ : state) {
+    int* element = container.resolve(handle);
+    benchmark::DoNotOptimize(element);
+  }
+}
+
+BENCHMARK(Resolve);
+
+static void EnumerateCallback(benchmark::State& state) {
+  thh::container_t<int> container;
+  std::vector<thh::handle_t> handles;
+  for (int i = 0; i < 10; ++i) {
+    handles.push_back(container.add());
+  }
+  for (auto _ : state) {
+    container.enumerate([i = 0](auto& element) mutable {
+      element = i++;
+    });
+  }
+}
+
+BENCHMARK(EnumerateCallback);
+
+static void EnumerateCallbackResolve(benchmark::State& state) {
+  thh::container_t<int> container;
+  std::vector<thh::handle_t> handles;
+  for (int i = 0; i < 10; ++i) {
+    handles.push_back(container.add());
+  }
+  for (auto _ : state) {
+    for (int i = 0; i < container.size(); ++i) {
+      int* element = container.resolve(handles[i]);
+      *element = i;
+    }
+  }
+}
+
+BENCHMARK_MAIN();
